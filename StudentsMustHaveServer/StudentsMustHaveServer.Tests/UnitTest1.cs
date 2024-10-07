45b5e83c4821d9e63abf2fac171d21c084d5ff15
mlaskowski7 <mtlaskowski7@gmail.com>
2024-10-07 17:50:55 +0200

    added tets project for NUnit

---
+++ StudentsMustHaveServer/StudentsMustHaveServer.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace StudentsMustHaveServer.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
