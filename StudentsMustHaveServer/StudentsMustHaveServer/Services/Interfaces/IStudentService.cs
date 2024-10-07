ef5fde55afe4cf55921f6eb5fd4c776ac808d9da
mlaskowski7 <mtlaskowski7@gmail.com>
2024-10-07 19:44:39 +0200

    student service interface

---
+++ StudentsMustHaveServer/StudentsMustHaveServer/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using StudentsMustHaveServer.Models;
+
+namespace StudentsMustHaveServer.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<bool> RegisterNewStudentAsync(string username, string password);
+        Task<bool> LoginStudentAsync(string username, string password);
+        Task<bool> DeleteAccountAsync(string username, string password);
+        Task<IEnumerable<Student>> GetAllStudentsAsync();
+
+    }
+}
